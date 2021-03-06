06f6262439df27fdc45b922ece263f166a457537
Andrew <anisimov.andrew.hi@gmail.com>
2021-03-06 20:16:53 +0300

    Testing dependency injection with "setter"

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,10 +1,18 @@
 package org.example;
 
 public class MusicPlayer {
-    private final Music music;
+    private Music music;
 
     //IoC
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {
+
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
