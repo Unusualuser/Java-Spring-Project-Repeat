af49e7cc6c13e68c52d3de178e3bc51dbc02011f
Andrew <anisimov.andrew.hi@gmail.com>
2021-03-08 00:12:49 +0300

    Creating the factory-method for "classicalMusicBean"

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,11 +1,18 @@
 package org.example;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic () {}
+
     public void doMyInit() {
         System.out.println("Do my initialization");
     }
+
     public void doMyDestroy() {
         System.out.println("Doing my destruction");
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
     }
 
     @Override
